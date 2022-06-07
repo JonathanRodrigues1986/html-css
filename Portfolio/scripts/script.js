ecac4bf88fe61826b3d334d4c1ef82e0b5ad12e4
JonathanRodrigues1986 <106286569+JonathanRodrigues1986@users.noreply.github.com>
2022-06-07 00:48:30 -0300

    criação portfolio

---
+++ Portfolio/scripts/script.js
@@ -0,0 +1,47 @@
+window.onload = () => {
+    const anchors = document.querySelectorAll('a');
+    const transition_el = document.querySelector('.transition');
+  
+    setTimeout(() => {
+      transition_el.classList.remove('is-active');
+    }, 500);
+  
+    for (let i = 0; i < anchors.length; i++) {
+      const anchor = anchors[i];
+  
+      anchor.addEventListener('click', e => {
+        e.preventDefault();
+        let target = e.target.href;
+  
+        console.log(transition_el);
+  
+        transition_el.classList.add('is-active');
+  
+        console.log(transition_el);
+  
+        setTimeout(() => {
+          window.location.href = target;
+        }, 500);
+      })
+    }
+  }
+
+  var clients = document.getElementById("clients");
+  var services = document.getElementById("services");
+  
+  clients.addEventListener(
+    "click",
+    function () {
+      $(clients).toggleClass("active");
+      $(".parent:not(#clients)").toggleClass("invisible");
+    },
+    false
+  );
+  services.addEventListener(
+    "click",
+    function () {
+      $(services).toggleClass("active");
+      $(".parent:not(#services)").toggleClass("invisible");
+    },
+    false
+  );  
