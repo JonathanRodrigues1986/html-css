105b9aeecbac2adbe465fcbe5c643af816790995
JonathanRodrigues1986 <106286569+JonathanRodrigues1986@users.noreply.github.com>
2022-06-07 01:11:03 -0300

    atualizaçoes

--- Portfolio/scripts/script.js
+++ Portfolio/scripts/script.js
@@ -26,22 +26,15 @@ window.onload = () => {
     }
   }
 
-  var clients = document.getElementById("clients");
-  var services = document.getElementById("services");
-  
-  clients.addEventListener(
-    "click",
-    function () {
-      $(clients).toggleClass("active");
-      $(".parent:not(#clients)").toggleClass("invisible");
-    },
-    false
-  );
-  services.addEventListener(
-    "click",
-    function () {
-      $(services).toggleClass("active");
-      $(".parent:not(#services)").toggleClass("invisible");
-    },
-    false
-  );  
+var clients = document.getElementById('clients');
+var services = document.getElementById('services');
+
+clients.addEventListener('click', function() {
+  $(clients).toggleClass("active");
+  $(".parent:not(#clients)").toggleClass("invisible");
+}, false);
+
+services.addEventListener('click', function() {
+  $(services).toggleClass("active");
+  $(".parent:not(#services)").toggleClass("invisible");
+}, false);
